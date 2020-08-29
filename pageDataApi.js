04f277be14f25242fba44c57070192f78f4d25d4
Deepika Ekka <deepika.ekka@sumtotalsystems.com>
2020-08-30 01:04:55 +0530

    Basic infinite scroll

---
+++ pageDataApi.js
@@ -0,0 +1,217 @@
+var data =  (function() {
+    "use strict"
+    var _pageData = [{
+        "page": 1,
+        "per_page": 6,
+        "total": 24,
+        "total_pages": 4,
+        "data": [
+            {
+                "id": 1,
+                "email": "michael.lawson@reqres.in",
+                "first_name": "Michael",
+                "last_name": "Lawson",
+                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg"
+            },
+            {
+                "id": 2,
+                "email": "lindsay.ferguson@reqres.in",
+                "first_name": "Lindsay",
+                "last_name": "Ferguson",
+                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/araa3185/128.jpg"
+            },
+            {
+                "id": 3,
+                "email": "tobias.funke@reqres.in",
+                "first_name": "Tobias",
+                "last_name": "Funke",
+                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/vivekprvr/128.jpg"
+            },
+            {
+                "id": 4,
+                "email": "byron.fields@reqres.in",
+                "first_name": "Byron",
+                "last_name": "Fields",
+                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/russoedu/128.jpg"
+            },
+            {
+                "id": 5,
+                "email": "george.edwards@reqres.in",
+                "first_name": "George",
+                "last_name": "Edwards",
+                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/mrmoiree/128.jpg"
+            },
+            {
+                "id": 6,
+                "email": "rachel.howell@reqres.in",
+                "first_name": "Rachel",
+                "last_name": "Howell",
+                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/hebertialmeida/128.jpg"
+            }]
+        }, {
+            "page": 2,
+            "per_page": 6,
+            "total": 24,
+            "total_pages": 4,
+            "data": [
+                {
+                    "id": 1,
+                    "email": "michael.lawsonII@reqres.in",
+                    "first_name": "MichaelII",
+                    "last_name": "Lawson",
+                    "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg"
+                },
+                {
+                    "id": 2,
+                    "email": "lindsay.ferguson@reqres.in",
+                    "first_name": "LindsayII",
+                    "last_name": "Ferguson",
+                    "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/araa3185/128.jpg"
+                },
+                {
+                    "id": 3,
+                    "email": "tobias.funke@reqres.in",
+                    "first_name": "TobiasII",
+                    "last_name": "Funke",
+                    "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/vivekprvr/128.jpg"
+                },
+                {
+                    "id": 4,
+                    "email": "byron.fields@reqres.in",
+                    "first_name": "ByronII",
+                    "last_name": "Fields",
+                    "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/russoedu/128.jpg"
+                },
+                {
+                    "id": 5,
+                    "email": "george.edwards@reqres.in",
+                    "first_name": "GeorgeII",
+                    "last_name": "Edwards",
+                    "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/mrmoiree/128.jpg"
+                },
+                {
+                    "id": 6,
+                    "email": "rachel.howell@reqres.in",
+                    "first_name": "RachelII",
+                    "last_name": "Howell",
+                    "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/hebertialmeida/128.jpg"
+                }]
+        }, {
+            
+        "page": 3,
+        "per_page": 6,
+        "total": 24,
+        "total_pages": 4,
+        "data": [
+            {
+                "id": 1,
+                "email": "michael.lawsonII@reqres.in",
+                "first_name": "MichaelII3",
+                "last_name": "Lawson",
+                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg"
+            },
+            {
+                "id": 2,
+                "email": "lindsay.ferguson@reqres.in",
+                "first_name": "LindsayII3",
+                "last_name": "Ferguson",
+                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/araa3185/128.jpg"
+            },
+            {
+                "id": 3,
+                "email": "tobias.funke@reqres.in",
+                "first_name": "TobiasII3",
+                "last_name": "Funke",
+                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/vivekprvr/128.jpg"
+            },
+            {
+                "id": 4,
+                "email": "byron.fields@reqres.in",
+                "first_name": "ByronII3",
+                "last_name": "Fields",
+                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/russoedu/128.jpg"
+            },
+            {
+                "id": 5,
+                "email": "george.edwards@reqres.in",
+                "first_name": "GeorgeII3",
+                "last_name": "Edwards",
+                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/mrmoiree/128.jpg"
+            },
+            {
+                "id": 6,
+                "email": "rachel.howell@reqres.in",
+                "first_name": "RachelII3",
+                "last_name": "Howell",
+                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/hebertialmeida/128.jpg"
+            }]
+        },{
+            
+        "page": 4,
+        "per_page": 6,
+        "total": 24,
+        "total_pages": 4,
+            "data": [
+                {
+                    "id": 1,
+                    "email": "michael.lawsonII@reqres.in",
+                    "first_name": "MichaelII4",
+                    "last_name": "Lawson",
+                    "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg"
+                },
+                {
+                    "id": 2,
+                    "email": "lindsay.ferguson@reqres.in",
+                    "first_name": "LindsayII4",
+                    "last_name": "Ferguson",
+                    "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/araa3185/128.jpg"
+                },
+                {
+                    "id": 3,
+                    "email": "tobias.funke@reqres.in",
+                    "first_name": "TobiasII4",
+                    "last_name": "Funke",
+                    "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/vivekprvr/128.jpg"
+                },
+                {
+                    "id": 4,
+                    "email": "byron.fields@reqres.in",
+                    "first_name": "ByronII4",
+                    "last_name": "Fields",
+                    "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/russoedu/128.jpg"
+                },
+                {
+                    "id": 5,
+                    "email": "george.edwards@reqres.in",
+                    "first_name": "GeorgeII4",
+                    "last_name": "Edwards",
+                    "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/mrmoiree/128.jpg"
+                },
+                {
+                    "id": 6,
+                    "email": "rachel.howell@reqres.in",
+                    "first_name": "RachelII4",
+                    "last_name": "Howell",
+                    "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/hebertialmeida/128.jpg"
+                }]
+        }
+    
+    ]    
+
+    function _getPageData(pageNumber) {
+        if(pageNumber > 4){
+            return undefined;
+        }
+        return _pageData[pageNumber-1];
+    }
+
+    return {
+        getPageData : function (x) {
+           return _getPageData.call(this,x);
+        }
+    }
+
+})();
+
+var d = data.getPageData(1);
+console.log(d);
